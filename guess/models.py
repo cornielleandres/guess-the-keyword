d012b264a5f22cc3271f056337db8b2a0d34d5e8
Carlos Vargas <cornielleandres@gmail.com>
2019-04-02 14:39:21 -0400

    Add CurrentGame to models.

--- guess/models.py
+++ guess/models.py
@@ -1,6 +1,10 @@
 from django.db					import models
 from django.contrib.auth.models	import User
 
+class CurrentGame(models.Model):
+	user = models.OneToOneField(User, on_delete = models.CASCADE)
+	search_term = models.TextField()
+
 class HighScore(models.Model):
 	user = models.OneToOneField(User, on_delete = models.CASCADE)
 	score = models.IntegerField(default = 0)
