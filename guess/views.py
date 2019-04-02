8b7d6ca64f992ab344d53e61ed735ea54265ad4a
Carlos Vargas <cornielleandres@gmail.com>
2019-04-02 00:45:58 -0400

    Connect view and template to custom search.

--- guess/views.py
+++ guess/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts	import render
+from decouple			import config
+import requests
 
 def index(request):
+	api_key = config('GOOGLE_API_KEY')
+	google_cs_id = config('GOOGLE_CS_ID')
+	search_term = 'house'
+	request_url = (
+		'https://www.googleapis.com/customsearch/v1?'
+		'q=' + search_term +
+		'&cx=' + google_cs_id +
+		'&exactTerms=' + search_term +
+		'&imgSize=medium'
+		'&searchType=image'
+		'&safe=active'
+		'&key=' + api_key
+	)
+	response = requests.get(request_url)
 	user = request.user
+	results = response.json()
+	print(results)
+	images = [ image['link'] for image in results['items'] ]
 	context = {
 		'user': user,
+		'images': images,
 	}
 	return render(request, 'guess/index.html', context)
