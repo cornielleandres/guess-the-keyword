986564d7675c8e0e19a1e3f5154ed39b16ec2238
Carlos Vargas <cornielleandres@gmail.com>
2019-04-04 20:40:00 -0400

    Update models to include points and strikes.

--- guess/models.py
+++ guess/models.py
@@ -4,7 +4,9 @@ from api.models					import CustomUser
 class CurrentGame(models.Model):
 	user = models.OneToOneField(CustomUser, on_delete = models.CASCADE)
 	search_term = models.TextField()
+	points = models.IntegerField(default = 0)
+	strikes = models.IntegerField(default = 0)
 
 class HighScore(models.Model):
-	user = models.OneToOneField(CustomUser, on_delete = models.CASCADE)
-	score = models.IntegerField(default = 0)
+	user = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
+	points = models.IntegerField(default = 0)
